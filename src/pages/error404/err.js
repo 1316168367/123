18a9f4c9be1de93556c41e3a21e82f875e9798f4
123 <123>
2020-12-04 22:27:33 +0800

    啊实打实

---
+++ src/pages/error404/err.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class err extends Component {
+    render() {
+        return (
+            <div>
+               <p>这是另外一个错误页面</p> 
+            </div>
+        )
+    }
+}
